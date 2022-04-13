61145037d73e319e411ee0255845186a5f7e34a1
Cory Hall <ComebackCory1@gmail.com>
2022-04-13 11:41:36 -0500

    added POST animal functionality

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const res = require('express/lib/response');
+const { animals } = require('./data/animals.json');
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { animals } = require('./data/animals.json');
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
    let personalityTraitsArray = [];
@@ -18,27 +25,28 @@ function filterByQuery(query, animalsArray) {
       } else {
          personalityTraitsArray = query.personalityTraits;
       }
+
+      // loop through each trait in the personalityTraits array
+      personalityTraitsArray.forEach(trait => {
+         // check the trait against each animal in the filteredResults array.
+         // remember, it is initially a copy of the animalsArray,
+         // but here we're updating it for each trait in the .forEach() loop.
+         // for each trait being targeted by the filter, the filteredResults
+         // array will then contain only the entries that contain the trait,
+         // so at the end we'll have an array of animals that have every one
+         // of the traits when the .forEach() loop is finished.
+         filteredResults = filteredResults.filter(
+            animal => animal.personalityTraits.indexOf(trait) !== -1
+         );
+      });
    }
-   // loop through each trait in the personalityTraits array
-   personalityTraitsArray.forEach(trait => {
-      // check the trait against each animal in the filteredResults array.
-      // remember, it is initially a copy of the animalsArray,
-      // but here we're updating it for each trait in the .forEach() loop.
-      // for each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
-      filteredResults = filteredResults.filter(
-         animal => animal.personalityTraits.indexOf(trait) !== -1
-      );
-   });
 
    if (query.diet) {
       filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
    }
 
    if (query.species) {
-      filteredResults = filteredResults.filter(animal => animal.species === query.species); 
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
    }
 
    if (query.name) {
@@ -54,9 +62,39 @@ function findById(id, animalsArray) {
    return result;
 };
 
+function createNewAnimal(body, animalsArray) {
+   const animal = body;
+   animalsArray.push(animal);
+   fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+   );
+
+   return animal;
+};
+
+function validateAnimal(animal) {
+   if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+   }
+
+   if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+   }
+
+   if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+   }
+
+   if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+   }
+   return true;
+}
+
 app.get('/api/animals', (req, res) => {
    let results = animals;
-   
+
    if (req.query) {
       results = filterByQuery(req.query, results);
    }
@@ -75,10 +113,17 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-   // req.body is where our incoming content will be
-   console.log(req.body);
-   req.json(req.body);
-})
+   // set id based on what the next index of the array will be
+   req.body.id = animals.length.toString();
+
+   // if any data in req.body is incorrect, send 400 error back
+   if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+   } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+   }
+});
 
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}`);
